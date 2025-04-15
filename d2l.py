4ecb94235185dbe16af80676ad29fa8366d6f7fa
Haokun.Feng <haokun.fung@gmail.com>
2025-04-15 20:46:37 +0000

    update

--- d2l.py
+++ d2l.py
@@ -3,6 +3,7 @@ from matplotlib_inline import backend_inline
 import time
 import numpy as np
 import torch
+from torch import nn
 import torchvision
 from torch.utils import data
 from torchvision import transforms
@@ -189,6 +190,25 @@ class d2l:
                 metric.add(d2l.accuracy(net(X), y), y.numel())
         return metric[0] / metric[1]
 
+    @staticmethod
+    def evaluate_accuracy_gpu(net, data_iter, device=None):
+        """Compute the accuracy for GPU."""
+        if isinstance(net, torch.nn.Module):
+            net.eval()
+            if not device:
+                device = next(iter(net.parameters())).device
+        metric = d2l.Accumulator(2)
+        with torch.no_grad():
+            for X, y in data_iter:
+                if isinstance(X, list):
+                    X = [x.to(device) for x in X]
+                else:
+                    X = X.to(device)
+                y = y.to(device)
+                metric.add(d2l.accuracy(net(X), y), y.numel())
+        return metric[0] / metric[1]
+        
+
     class Accumulator:
         """Accumulate the sum and count."""
         
@@ -235,6 +255,45 @@ class d2l:
         assert train_loss < 0.5, train_loss
         assert train_acc <= 1 and train_acc > 0.7, train_acc
         assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+    @staticmethod
+    def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
+        """Train a model with GPU support."""
+        def init_weights(m):
+            if type(m) == nn.Linear or type(m) == nn.Conv2d:
+                nn.init.xavier_uniform_(m.weight)
+        net.apply(init_weights)
+        print('training on', device)
+        net.to(device)
+        optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+        loss = nn.CrossEntropyLoss()
+        animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], legend=['train loss', 'train acc', 'test acc'])
+        timer, num_batches = d2l.Timer(), len(train_iter)
+        for epoch in range(num_epochs):
+            metric = d2l.Accumulator(3)
+            net.train()
+            for i, (X, y) in enumerate(train_iter):
+                timer.start()
+                optimizer.zero_grad()
+                X, y = X.to(device), y.to(device)
+                y_hat = net(X)
+                l = loss(y_hat, y)
+                l.backward()
+                optimizer.step()
+                with torch.no_grad():
+                    metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+                timer.stop()
+                train_loss = metric[0] / metric[2]
+                train_acc= metric[1] / metric[2]
+                if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                    animator.add(epoch + (i + 1) / num_batches, (train_loss, train_acc, None))
+            test_acc = d2l.evaluate_accuracy_gpu(net, test_iter)
+            animator.add(epoch + 1, (None, None, test_acc))
+        print(f'loss {train_loss:.3f}, train acc {train_acc:.3f}, '
+              f'test acc {test_acc:.3f}')
+        print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+              f'on {str(device)}')
+
     
     @staticmethod
     def predict_ch3(net, test_iter, n=6):
@@ -295,3 +354,27 @@ class d2l:
             self.config_axes()
             display.display(self.fig)
             display.clear_output(wait=True)
+
+    @staticmethod
+    def corr2d(X, K):
+        """Compute the 2D cross-correlation."""
+        h, w = K.shape
+        Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+        for i in range(Y.shape[0]):
+            for j in range(Y.shape[1]):
+                Y[i, j] = (X[i:i + h, j:j + w] * K).sum()
+        return Y
+    
+    @staticmethod
+    def try_gpu(i=0):
+        """Try to use GPU if available."""
+        if torch.cuda.device_count() >= i + 1:
+            return torch.device(f'cuda:{i}')
+        return torch.device('cpu')
+    
+    @staticmethod
+    def try_all_gpus():
+        """Try to use all available GPUs."""
+        devices = [torch.device(f'cuda:{i}')
+                   for i in range(torch.cuda.device_count())]
+        return devices if devices else [torch.device('cpu')]
