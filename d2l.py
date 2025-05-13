e38723d1e73542ef30f53ad6343d29943dd88bcf
Haokun.Feng <haokun.fung@gmail.com>
2025-05-13 02:07:16 +0000

    update attention and transformer

--- d2l.py
+++ d2l.py
@@ -422,3 +422,31 @@ class d2l:
                 X = self.conv3(X)
             Y += X
             return F.relu(Y)
+
+    # ----------------------------------------------------------------------------------------
+    # attention mechanism
+    def show_heatmaps(matrices,
+                      xlabel,
+                      ylabel,
+                      titles=None,
+                      figsize=(2.5, 2.5),
+                      cmap='Reds'):
+        """ Display matrics heatmaps """
+        d2l.use_svg_display()
+        num_rows, num_cols = matrices.shape[0], matrices.shape[1]
+        fig, axes = plt.subplots(num_rows,
+                                 num_cols,
+                                 figsize=figsize,
+                                 sharex=True,
+                                 sharey=True,
+                                 squeeze=False)
+        for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+            for j, (ax, matrix) in enumerate(zip(row_axes, row_matrices)):
+                pcm = ax.imshow(matrix.detach().numpy(), cmap=cmap)
+                if i == num_rows - 1:
+                    ax.set_xlabel(xlabel)
+                if j == 0:
+                    ax.set_ylabel(ylabel)
+                if titles:
+                    ax.set_title(titles[j])
+        fig.colorbar(pcm, ax=axes, shrink=0.6)
